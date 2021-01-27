db8465d6257e6a902147a323ec40d1e7d28ae087
Flone <flonednb@gmail.com>
2021-01-27 20:49:02 +0500

    Add memory check for debug builds.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,11 @@
 
 int main(int argc, char *argv[])
 {
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG) || defined(_DEBUG)
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
     QApplication a(argc, argv);
 
     MainWindow w;
